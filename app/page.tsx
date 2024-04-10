34fa1623ed1ca910c25c8f487471673351258dc4
shbhmj <shbhmkmrjha@gmail.com>
2024-04-10 18:05:50 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,16 @@ const shortify = (text: string, maxLength = 60) => {
   }
 
   return text.substring(0, maxLength) + " ...";
-}
+};
 
-const Page: NextPage = () => {
+const Page = () => {
   const blogs = use(getInitialBlogs());
-  
+
   return (
     <div>
       <h2 className="sr-only">Blogs</h2>
-
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {blogs.map(blog => (
+        {blogs.map((blog) => (
           <Link key={blog.slug} href={`/blogs/${blog.slug}`} className="group">
             <div className="relative aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
               <Image
@@ -36,12 +35,14 @@ const Page: NextPage = () => {
               />
             </div>
             <h3 className="mt-4 text-sm text-gray-700">{blog.title}</h3>
-            <p className="mt-1 text-md font-medium text-gray-900">{shortify(blog.description, 100)}</p>
+            <p className="mt-1 text-md font-medium text-gray-900">
+              {shortify(blog.description, 100)}
+            </p>
           </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
